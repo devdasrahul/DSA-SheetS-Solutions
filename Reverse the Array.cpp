da2c533f4e50761ff8d17a51bfea1d99f3c821ba
Rahul <rahuld07678@gmail.com>
2025-03-17 22:32:40 +0530

    Create Reverse the Array.cpp

---
+++ Reverse the Array.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Function to reverse an array
+void reverseArray(vector<int> &arr) {
+    int left = 0, right = arr.size() - 1;
+    while (left < right) {
+        swap(arr[left], arr[right]);
+        left++;
+        right--;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+    
+    vector<int> arr(n);
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    
+    reverseArray(arr);
+    
+    cout << "Reversed array: ";
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
